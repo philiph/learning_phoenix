5c0dd353b9d66799b1054bbff7abcf2b850647e1
Philip Hagen <phil@philiphagen.com>
2020-07-13 13:53:29 +0800

    Ping endpoint - demonstrate custom status & resp headers

--- lib/learning_phoenix_web/controllers/api/ping_controller.ex
+++ lib/learning_phoenix_web/controllers/api/ping_controller.ex
@@ -2,7 +2,10 @@ defmodule LearningPhoenixWeb.Api.PingController do
   use LearningPhoenixWeb, :controller
 
   def index(conn, _params) do
-    json(conn, %{
+    conn
+    |> put_status(200)
+    |> put_resp_header("content-type", "application/json")
+    |> json(%{
       hello: "world"
     })
   end
