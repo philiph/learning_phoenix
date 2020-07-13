1a94c97c547b3f4ce8b203c15839fc13450ac532
Philip Hagen <phil@philiphagen.com>
2020-07-13 13:27:29 +0800

    Accept name query parameter and display in the view

--- lib/learning_phoenix_web/controllers/hello_controller.ex
+++ lib/learning_phoenix_web/controllers/hello_controller.ex
@@ -1,7 +1,11 @@
 defmodule LearningPhoenixWeb.HelloController do
   use LearningPhoenixWeb, :controller
 
+  def index(conn, %{"name" => name}) do
+    render(conn, "index.html", name: name)
+  end
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", name: "Stranger")
   end
 end
